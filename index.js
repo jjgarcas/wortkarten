67528e93f38dd4b0c68284b1daf619034dfd3519
jjgarcas <jjgcastela@gmail.com>
2023-12-08 17:12:34 +0100

    Added settings

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ function init() {
     const cardSingular = document.getElementById('cardSingular');
     const cardPlural = document.getElementById('cardPlural');
     const cardNext = document.getElementById('cardNext');
+    const settingsIcon = document.getElementById('settingsIcon');
+    const settingsContainer = document.getElementById('settingsContainer');
+
+    let settingsOpened = false;
+    let lessons;
+    let allLessonsToggle;
+    let disabledLessons = JSON.parse(localStorage.getItem('disabledLessons') || '[]');
+    let prevDisabledLessons = [...disabledLessons];
+    const lessonToggles = [];
 
     const articles = {
         m: 'der',
@@ -20,11 +29,12 @@ function init() {
     };
 
     const initializeCards = () => {
-        remainingWords = [...words];
+        remainingWords = words.filter(({ lesson }) => !disabledLessons.includes(lesson));
     };
 
     const updateCard = (ev) => {
         ev?.stopPropagation();
+        ev?.preventDefault();
         if (!remainingWords?.length) initializeCards();
         const index = Math.floor(Math.random() * remainingWords.length);
         const currentWord = remainingWords.splice(index, 1)[0];
@@ -35,8 +45,95 @@ function init() {
         container.classList.add(`genre_${currentWord.genre}`);
     };
 
+    const toggleSettings = (ev) => {
+        ev?.stopPropagation();
+        ev?.preventDefault();
+        if (settingsOpened) {
+            settingsOpened = false;
+            settingsIcon.classList.remove('closeIcon');
+            settingsContainer.classList.add('hidden');
+            if (prevDisabledLessons.length !== disabledLessons.length || disabledLessons.some(lesson => !prevDisabledLessons.includes(lesson))) {
+                console.log('new disabled lessons');
+                prevDisabledLessons = [...disabledLessons];
+                localStorage.setItem('disabledLessons', JSON.stringify(disabledLessons));
+                initializeCards();
+                updateCard();
+            }
+        } else {
+            settingsOpened = true;
+            settingsIcon.classList.add('closeIcon');
+            settingsContainer.classList.remove('hidden');
+        }
+    };
+
+    const getLessonDisabled = (lesson) => {
+        if (lesson === 'all') return disabledLessons.length > 0;
+        return disabledLessons.includes(lesson);
+    };
+
+    const toggleLesson = (lesson, ev) => {
+        ev?.stopPropagation();
+        ev?.preventDefault();
+        const isDisabled = getLessonDisabled(lesson);
+        if (lesson === 'all') {
+            if (isDisabled) {
+                disabledLessons = [];
+                allLessonsToggle.classList.remove('disabled');
+                lessonToggles.forEach(lessonToggle => lessonToggle.classList.remove('disabled'));
+            }
+        } else if (isDisabled) {
+            disabledLessons = disabledLessons.filter(value => value !== lesson);
+            lessonToggles.find(lessonToggle => lessonToggle.id === `lesson${lesson}`).classList.remove('disabled');
+            if (getLessonDisabled('all')) {
+                allLessonsToggle.classList.add('disabled');
+            } else {
+                allLessonsToggle.classList.remove('disabled');
+            }
+        } else if (disabledLessons.length < lessons.length - 1) {
+            disabledLessons.push(lesson);
+            lessonToggles.find(lessonToggle => lessonToggle.id === `lesson${lesson}`).classList.add('disabled');
+            allLessonsToggle.classList.add('disabled');
+        }
+    };
+
+    const addSettings = () => {
+        lessons = words.reduce((acc, value) => {
+            if (!acc.includes(value.lesson)) acc.push(value.lesson);
+            return acc;
+        }, []);
+        const container = document.createElement('div');
+        container.className = 'lessonContainer';
+        const lessonName = document.createElement('div');
+        lessonName.className = 'lessonName';
+        lessonName.innerText = 'Alle Themen';
+        container.appendChild(lessonName);
+        const lessonToggle = document.createElement('div');
+        lessonToggle.className = `lessonToggle${getLessonDisabled('all') ? ' disabled' : ''}`;
+        lessonToggle.addEventListener('click', toggleLesson.bind(null, 'all'));
+        allLessonsToggle = lessonToggle;
+        container.appendChild(lessonToggle);
+        settingsContainer.appendChild(container);
+        lessons.forEach((lesson) => {
+            const container = document.createElement('div');
+            container.className = 'lessonContainer';
+            const lessonName = document.createElement('div');
+            lessonName.className = 'lessonName';
+            lessonName.innerText = `Thema ${lesson}`;
+            container.appendChild(lessonName);
+            const lessonToggle = document.createElement('div');
+            lessonToggle.className = `lessonToggle${getLessonDisabled(lesson) ? ' disabled' : ''}`;
+            lessonToggle.id = `lesson${lesson}`;
+            lessonToggle.addEventListener('click', toggleLesson.bind(null, lesson));
+            lessonToggles.push(lessonToggle);
+            container.appendChild(lessonToggle);
+            settingsContainer.appendChild(container);
+        });
+    };
+
     cardFront.addEventListener('click', revealCard);
     cardNext.addEventListener('click', updateCard);
+    settingsIcon.addEventListener('click', toggleSettings);
 
+    addSettings();
     updateCard();
 }
