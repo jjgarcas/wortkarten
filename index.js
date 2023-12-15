37d7de83043ea3ebabda594367cfc134d2bc71ea
jjgarcas <jjgcastela@gmail.com>
2023-12-15 12:18:27 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function init() {
 
+    const flags = location.search.substring(1).split('&').filter(Boolean).reduce((acc, value) => {
+        const [key, val = 'true'] = value.split('=');
+        acc[key] = val;
+        return acc;
+    }, {});
+
+    console.log('flags', flags);
+
     const container = document.getElementById('container');
     const imgContainer = document.getElementById('imgContainer');
     const cardFront = document.getElementById('cardFront');
@@ -8,13 +16,25 @@ function init() {
     const cardNext = document.getElementById('cardNext');
     const settingsIcon = document.getElementById('settingsIcon');
     const settingsContainer = document.getElementById('settingsContainer');
+    const imagesToggle = document.getElementById('imagesToggle');
 
     let settingsOpened = false;
+    let disabledImages = localStorage.getItem('disabledImages') === 'true';
+    let prevDisabledImages = disabledImages;
     let lessons;
     let allLessonsToggle;
     let disabledLessons = JSON.parse(localStorage.getItem('disabledLessons') || '[]');
     let prevDisabledLessons = [...disabledLessons];
     const lessonToggles = [];
+
+    if (flags.bookColors === 'true') {
+        container.classList.add('bookColors');
+    }
+
+    if (disabledImages) {
+        imgContainer.classList.add('disabledImages');
+        imagesToggle.classList.add('disabled');
+    }
 
     const articles = {
         m: 'der',
@@ -38,7 +58,8 @@ function init() {
         if (!remainingWords?.length) initializeCards();
         const index = Math.floor(Math.random() * remainingWords.length);
         const currentWord = remainingWords.splice(index, 1)[0];
-        imgContainer.style.backgroundImage = `url('./assets/${currentWord.id}.png')`;
+        imgContainer.style.backgroundImage = `url('./assets/${currentWord.image}.png')`;
+        imgContainer.innerText = currentWord.translation;
         cardSingular.innerText = `${articles[currentWord.genre]} ${currentWord.singular}${currentWord.plural ? ',' : ''}`;
         cardPlural.innerText = currentWord.plural ? `die ${currentWord.plural}` : '';
         container.classList.remove('reveal', 'genre_f', 'genre_m', 'genre_n');
@@ -53,11 +74,19 @@ function init() {
             settingsIcon.classList.remove('closeIcon');
             settingsContainer.classList.add('hidden');
             if (prevDisabledLessons.length !== disabledLessons.length || disabledLessons.some(lesson => !prevDisabledLessons.includes(lesson))) {
-                console.log('new disabled lessons');
                 prevDisabledLessons = [...disabledLessons];
                 localStorage.setItem('disabledLessons', JSON.stringify(disabledLessons));
                 initializeCards();
                 updateCard();
+            }
+            if (prevDisabledImages !== disabledImages) {
+                prevDisabledImages = disabledImages;
+                localStorage.setItem('disabledImages', String(disabledImages));
+                if (disabledImages) {
+                    imgContainer.classList.add('disabledImages');
+                } else {
+                    imgContainer.classList.remove('disabledImages');
+                }
             }
         } else {
             settingsOpened = true;
@@ -69,6 +98,18 @@ function init() {
     const getLessonDisabled = (lesson) => {
         if (lesson === 'all') return disabledLessons.length > 0;
         return disabledLessons.includes(lesson);
+    };
+
+    const toggleImages = (ev) => {
+        ev?.stopPropagation();
+        ev?.preventDefault();
+        if (disabledImages) {
+            disabledImages = false;
+            imagesToggle.classList.remove('disabled');
+        } else {
+            disabledImages = true;
+            imagesToggle.classList.add('disabled');
+        }
     };
 
     const toggleLesson = (lesson, ev) => {
@@ -97,42 +138,44 @@ function init() {
     };
 
     const addSettings = () => {
+        const lessonsContainer = document.getElementById('lessonsContainer');
         lessons = words.reduce((acc, value) => {
             if (!acc.includes(value.lesson)) acc.push(value.lesson);
             return acc;
         }, []);
         const container = document.createElement('div');
-        container.className = 'lessonContainer';
+        container.className = 'toggleContainer';
         const lessonName = document.createElement('div');
-        lessonName.className = 'lessonName';
+        lessonName.className = 'toggleName';
         lessonName.innerText = 'Alle Themen';
         container.appendChild(lessonName);
         const lessonToggle = document.createElement('div');
-        lessonToggle.className = `lessonToggle${getLessonDisabled('all') ? ' disabled' : ''}`;
+        lessonToggle.className = `toggle${getLessonDisabled('all') ? ' disabled' : ''}`;
         lessonToggle.addEventListener('click', toggleLesson.bind(null, 'all'));
         allLessonsToggle = lessonToggle;
         container.appendChild(lessonToggle);
-        settingsContainer.appendChild(container);
+        lessonsContainer.appendChild(container);
         lessons.forEach((lesson) => {
             const container = document.createElement('div');
-            container.className = 'lessonContainer';
+            container.className = 'toggleContainer';
             const lessonName = document.createElement('div');
-            lessonName.className = 'lessonName';
+            lessonName.className = 'toggleName';
             lessonName.innerText = `Thema ${lesson}`;
             container.appendChild(lessonName);
             const lessonToggle = document.createElement('div');
-            lessonToggle.className = `lessonToggle${getLessonDisabled(lesson) ? ' disabled' : ''}`;
+            lessonToggle.className = `toggle${getLessonDisabled(lesson) ? ' disabled' : ''}`;
             lessonToggle.id = `lesson${lesson}`;
             lessonToggle.addEventListener('click', toggleLesson.bind(null, lesson));
             lessonToggles.push(lessonToggle);
             container.appendChild(lessonToggle);
-            settingsContainer.appendChild(container);
+            lessonsContainer.appendChild(container);
         });
     };
 
     cardFront.addEventListener('click', revealCard);
     cardNext.addEventListener('click', updateCard);
     settingsIcon.addEventListener('click', toggleSettings);
+    imagesToggle.addEventListener('click', toggleImages);
 
     addSettings();
     updateCard();
